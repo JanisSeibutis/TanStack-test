7df4c0c681eba689a48a2f17b8bbe833464ed4ae
JanisSeibutis <janis.seibutis@gmail.com>
2025-10-23 21:22:08 +0200

    feat: create bug form

---
+++ src/components/forms/Test-form.tsx
@@ -0,0 +1,252 @@
+import { useForm } from '@tanstack/react-form'
+import { toast } from 'sonner'
+import * as z from 'zod'
+
+import { Checkbox } from '../ui/checkbox'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroupTextarea,
+} from '@/components/ui/input-group'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
+const formSchema = z.object({
+  title: z
+    .string()
+    .min(5, 'Bug title must be at least 5 characters.')
+    .max(32, 'Bug title must be at most 32 characters.'),
+  description: z
+    .string()
+    .min(20, 'Description must be at least 20 characters.')
+    .max(100, 'Description must be at most 100 characters.'),
+  isActive: z.boolean(),
+  bugType: z.enum(
+    ['UI/UX', 'Functionality', 'Performance', 'Security'],
+    'Please select an option',
+  ),
+})
+
+type FormData = {
+  title: string
+  description: string
+  isActive: boolean
+  bugType: '' | 'UI/UX' | 'Functionality' | 'Performance' | 'Security'
+}
+
+export const TestForm = () => {
+  const form = useForm({
+    defaultValues: {
+      title: '',
+      description: '',
+      isActive: true,
+      bugType: '',
+    },
+    validators: {
+      onSubmit: formSchema,
+    },
+    onSubmit: ({ value }: { value: FormData }) => {
+      toast('You submitted the following values:', {
+        description: (
+          <pre className="bg-code text-code-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4">
+            <code>{JSON.stringify(value, null, 2)}</code>
+          </pre>
+        ),
+        position: 'bottom-right',
+        classNames: {
+          content: 'flex flex-col gap-2',
+        },
+        style: {
+          '--border-radius': 'calc(var(--radius)  + 4px)',
+        } as React.CSSProperties,
+      })
+    },
+  })
+  return (
+    <Card className="w-full sm:max-w-md">
+      <CardHeader>
+        <CardTitle>Bug Report</CardTitle>
+        <CardDescription>
+          Help us improve by reporting bugs you encounter.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form
+          id="bug-report-form"
+          onSubmit={(e) => {
+            e.preventDefault()
+            form.handleSubmit()
+          }}
+        >
+          <FieldGroup>
+            <form.Field
+              name="title"
+              children={(field) => {
+                const isInvalid =
+                  field.state.meta.isTouched && !field.state.meta.isValid
+                return (
+                  <Field data-invalid={isInvalid}>
+                    <FieldLabel htmlFor={field.name}>Bug Title</FieldLabel>
+                    <Input
+                      id={field.name}
+                      name={field.name}
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                      aria-invalid={isInvalid}
+                      placeholder="Login button not working on mobile"
+                      autoComplete="off"
+                    />
+                    {isInvalid && (
+                      <FieldError errors={field.state.meta.errors} />
+                    )}
+                  </Field>
+                )
+              }}
+            />
+            <form.Field
+              name="description"
+              children={(field) => {
+                const isInvalid =
+                  field.state.meta.isTouched && !field.state.meta.isValid
+                return (
+                  <Field data-invalid={isInvalid}>
+                    <FieldLabel htmlFor={field.name}>Description</FieldLabel>
+                    <InputGroup>
+                      <InputGroupTextarea
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        placeholder="I'm having an issue with the login button on mobile."
+                        rows={6}
+                        className="min-h-24 resize-none"
+                        aria-invalid={isInvalid}
+                      />
+                      <InputGroupAddon align="block-end">
+                        <InputGroupText className="tabular-nums">
+                          {field.state.value.length}/100 characters
+                        </InputGroupText>
+                      </InputGroupAddon>
+                    </InputGroup>
+                    <FieldDescription>
+                      Include steps to reproduce, expected behavior, and what
+                      actually happened.
+                    </FieldDescription>
+                    {isInvalid && (
+                      <FieldError errors={field.state.meta.errors} />
+                    )}
+                  </Field>
+                )
+              }}
+            />
+            <form.Field
+              name="isActive"
+              children={(field) => {
+                return (
+                  <FieldGroup>
+                    <Field orientation={'horizontal'}>
+                      <Checkbox
+                        defaultChecked
+                        id={field.name}
+                        name={field.name}
+                        checked={field.state.value}
+                        onBlur={field.handleBlur}
+                        onCheckedChange={(checked) =>
+                          field.handleChange(Boolean(checked))
+                        }
+                      />
+                      <FieldLabel htmlFor={field.name}>
+                        Is the Bug active
+                      </FieldLabel>
+                    </Field>
+                  </FieldGroup>
+                )
+              }}
+            ></form.Field>
+            <form.Field
+              name="bugType"
+              children={(field) => {
+                const isInvalid =
+                  field.state.meta.isTouched && !field.state.meta.isValid
+                return (
+                  <FieldGroup>
+                    <Field data-invalid={isInvalid}>
+                      <FieldLabel htmlFor={field.name}>Bug Type</FieldLabel>
+                      <Select
+                        name={field.name}
+                        value={field.state.value}
+                        onValueChange={(value) =>
+                          field.handleChange(value as FormData['bugType'])
+                        }
+                      >
+                        <SelectTrigger
+                          aria-invalid={isInvalid}
+                          className={`w-[180px] group-data-[invalid=true]:border-red-500 group-data-[invalid=true]:ring-red-500`}
+                        >
+                          <SelectValue placeholder="Select a bug type" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectGroup>
+                            <SelectLabel>Bug types</SelectLabel>
+                            <SelectItem value="UI/UX">UI/UX</SelectItem>
+                            <SelectItem value="Functionality">
+                              Functionality
+                            </SelectItem>
+                            <SelectItem value="Performance">
+                              Performance
+                            </SelectItem>
+                            <SelectItem value="Security">Security</SelectItem>
+                          </SelectGroup>
+                        </SelectContent>
+                      </Select>
+                      {isInvalid && (
+                        <FieldError errors={field.state.meta.errors} />
+                      )}
+                    </Field>
+                  </FieldGroup>
+                )
+              }}
+            ></form.Field>
+          </FieldGroup>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <Field orientation="horizontal">
+          <Button type="button" variant="outline" onClick={() => form.reset()}>
+            Reset
+          </Button>
+          <Button type="submit" form="bug-report-form">
+            Submit
+          </Button>
+        </Field>
+      </CardFooter>
+    </Card>
+  )
+}
