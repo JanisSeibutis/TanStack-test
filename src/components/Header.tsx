25f5e90777e3ea12b46e9a8ecacfec9c7aa6abe8
JanisSeibutis <janis.seibutis@gmail.com>
2025-10-24 23:26:27 +0200

    feat: add query to check user status

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,35 @@
-import { Link } from '@tanstack/react-router'
+import { Link, useNavigate } from '@tanstack/react-router'
+import { useEffect } from 'react'
+import { useQueryClient } from '@tanstack/react-query'
+
+import { Button } from './ui/button'
+import { supabase } from '@/supabaseClient'
 
 export const Header = () => {
+  const navigate = useNavigate()
+  const handleLogout = async () => {
+    await supabase.auth.signOut()
+    navigate({ to: '/' })
+  }
+  const queryClient = useQueryClient()
+  useEffect(() => {
+    const { data: listener } = supabase.auth.onAuthStateChange(() => {
+      // invalidate user query
+      queryClient.invalidateQueries({ queryKey: ['user'] })
+    })
+    return () => listener.subscription.unsubscribe()
+  }, [queryClient])
+
   return (
     <header className="flex gap-5 pl-40 h-10 items-center bg-green-400 text-lg font-bold">
       <Link to="/">Home</Link>
       <Link to="/reports">Reports</Link>
-      <Link to="/login" className="ml-auto mr-10">
+      <Button
+        className="ml-auto mr-10 bg-transparent hover:bg-transparent hover:underline text-lg font-bold text-black cursor-pointer"
+        onClick={handleLogout}
+      >
         Log out
-      </Link>
+      </Button>
     </header>
   )
 }
--- src/routes/reports/index.tsx
+++ src/routes/reports/index.tsx
@@ -1,11 +1,40 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useQuery } from '@tanstack/react-query'
+import { supabase } from '@/supabaseClient'
+
 import { TestForm } from '@/components/forms/Test-form'
+import { Login } from '@/components/Login'
 
 export const Route = createFileRoute('/reports/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const { data: user, isLoading } = useQuery({
+    queryKey: ['user'],
+    queryFn: async () => {
+      const { data } = await supabase.auth.getUser()
+      return data.user
+    },
+    staleTime: Infinity,
+  })
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        Loading...
+      </div>
+    )
+  }
+
+  if (!user) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <Login />
+      </div>
+    )
+  }
+
   return (
     <div className="flex items-center justify-center min-h-screen">
       <TestForm />
